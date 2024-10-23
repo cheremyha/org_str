4289dff06c27ab2da570a5b90df875116b03d454
cheremyha <liubchenko.nikita@yandex.ru>
2024-10-23 18:00:38 +0300

    Deleted WhiteNoiseMiddleware

--- org_str/settings.py
+++ org_str/settings.py
@@ -56,8 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # A New one
-  	'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'org_str.urls'
